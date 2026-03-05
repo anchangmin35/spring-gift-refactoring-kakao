dde163b853f3ec4911a1290f38904f4644f4ab18
paul.an <chm20060@gmail.com>
2026-03-05 14:42:20 +0900

    refactor: Wish 소유권 검증 로직을 도메인 엔티티로 이동

--- src/main/java/gift/wish/Wish.java
+++ src/main/java/gift/wish/Wish.java
@@ -42,4 +42,8 @@ public class Wish {
     public Product getProduct() {
         return product;
     }
+
+    public boolean isOwnedBy(Long memberId) {
+        return this.memberId.equals(memberId);
+    }
 }
--- src/main/java/gift/wish/WishService.java
+++ src/main/java/gift/wish/WishService.java
@@ -47,7 +47,7 @@ public class WishService {
     public void removeWish(Long memberId, Long wishId) {
         Wish wish = wishRepository.findById(wishId)
             .orElseThrow(() -> new NoSuchElementException("위시가 존재하지 않습니다. id=" + wishId));
-        if (!wish.getMemberId().equals(memberId)) {
+        if (!wish.isOwnedBy(memberId)) {
             throw new ForbiddenException("해당 위시에 대한 권한이 없습니다.");
         }
         wishRepository.delete(wish);
